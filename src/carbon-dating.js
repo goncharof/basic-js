f72f19a4668256c114112ceb1cd9ff6b393d7883
yolo <igoncharov@flightpath3d.com>
2025-02-12 16:13:51 +0000

    feat: implement carbon-14 dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,12 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') return false;
+  if (!/^-?\d+(\.\d+)?$/.test(sampleActivity)) return false;
+  const activity = Number(sampleActivity);
+  if (activity <= 0 || activity > MODERN_ACTIVITY) return false;
+  return Math.ceil(Math.log(MODERN_ACTIVITY / activity) / Math.log(2) / (1 / HALF_LIFE_PERIOD));
 }
 
 module.exports = {
