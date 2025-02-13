910ad13ad284485645ee6fb94c2c8aa200bd6b16
yolo <igoncharov@flightpath3d.com>
2025-02-13 23:34:27 +0000

    feat: implement what-season

--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if (date === undefined) {
+    return 'Unable to determine the time of year!';
+  }
+
+  if (!(date instanceof Date)) {
+    throw new Error('Invalid date!');
+  }
+
+  if (date.getTime) {
+    try {
+      date.getTime().toString();
+    } catch (e) {
+      if (e.message === 'this is not a Date object.')
+        throw new Error('Invalid date!');
+      throw e;
+    }
+  }
+
+  const month = date.getMonth();
+  
+  const season = month >= 2 && month <= 4 
+    ? 'spring' 
+    : month >= 5 && month <= 7 
+      ? 'summer' 
+      : month >= 8 && month <= 10 
+        ? 'autumn' 
+        : 'winter';
+
+  return season;  
 }
 
 module.exports = {
