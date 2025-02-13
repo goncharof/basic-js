9aa2e133cf039e0ade984862b43997545cb9cb70
yolo <igoncharov@flightpath3d.com>
2025-02-13 18:46:30 +0000

    feat: implement transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if(!Array.isArray(arr))
+    throw new Error('\'arr\' parameter must be an instance of the Array!');
+
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case '--double-next':
+        if(i === arr.length - 1) continue;
+        result.push(arr[i + 1]);
+        break;
+      case '--double-prev':
+        if(i === 0 || arr[i - 1] === undefined) continue;
+        result.push(arr[i - 1]);
+        break;
+      case '--discard-prev':
+        if(i === 0 || arr[i - 1] === undefined) continue;
+        result.pop();
+        break;
+      case '--discard-next':
+        i++;
+        arr[i] = undefined;
+        break;
+      default:
+        result.push(arr[i]);
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
