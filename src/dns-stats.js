eee5cddfbc10c9e03d363c5a476a892e7f1e38e4
yolo <igoncharov@flightpath3d.com>
2025-02-13 15:37:53 +0000

    feat: implement dns stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  if (!Array.isArray(domains)) return {};
+
+  const stats = {};
+
+  domains.forEach(domain => {
+    const parts = domain.split('.').reverse();
+    const topLevelDomain = '.' + parts[0];
+    stats[topLevelDomain] = (stats[topLevelDomain] || 0) + 1;
+
+    for (let i = 1; i < parts.length; i++) {
+      const currentDomain = parts.slice(0, i + 1).join('.');
+      const key = '.' + currentDomain;
+      stats[key] = (stats[key] || 0) + 1;
+    }
+  });
+
+  return stats;
 }
 
 module.exports = {
