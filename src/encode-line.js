5a9127c0bc931e4047aef3e30613bac8519a7660
yolo <igoncharov@flightpath3d.com>
2025-02-13 15:40:19 +0000

    feat: implement encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  if (typeof str !== 'string') return false;
+  
+  if (str.length === 0) return '';
+  
+  let encoded = '';
+  let count = 1;
+  
+  for (let i = 1; i <= str.length; i++) {
+    if (i < str.length && str[i] === str[i - 1]) {
+      count++;
+    } else {
+      encoded += count > 1 ? count + str[i - 1] : str[i - 1];
+      count = 1;
+    }
+  }
+  
+  return encoded;
 }
 
 module.exports = {
